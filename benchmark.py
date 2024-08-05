fa234d99fe61eeb138b26fa4ebbd7c438d3d43ae
david.algis <david.algis@studio-nyx.com>
2024-08-05 19:17:02 +0200

    🔨 Update script benchmark for config file

--- benchmark.py
+++ benchmark.py
@@ -3,6 +3,7 @@ import subprocess
 import argparse
 import sys
 import shutil
+import json
 
 
 def run_command(*args, **kwargs):
@@ -47,6 +48,140 @@ def print_red(*args, **kwargs):
     print("\033[91m", *args, "\033[0m", **kwargs)
 
 
+def create_config_file(build_dir, num_samples):
+    config_path = os.path.join(build_dir, 'Config.json')
+    config = {
+        'NumSamplesPerSize': num_samples
+    }
+    with open(config_path, 'w') as file:
+        json.dump(config, file, indent=4)
+    print_green(f"Config file created: {config_path}")
+
+
+def package_interop(script_dir):
+    print_green("Package InteropUnityCUDA...")
+    interop_unity_cuda_dir = os.path.join(
+        script_dir, 'PluginBenchmark', 'Vendors', 'InteropUnityCUDA')
+    package_script_path = os.path.join(
+        interop_unity_cuda_dir, 'Plugin', 'buildtools', 'packageUnity.py')
+    if os.path.exists(interop_unity_cuda_dir):
+        packaged_args = ['python', package_script_path]
+        print_green("Execute package script: ", packaged_args)
+        run_command(packaged_args, cwd=script_dir)
+
+
+def generate_compile_plugin(script_dir, cuda_path):
+    print_green("Configure Benchmark Plugin...")
+    project_dir = os.path.join(script_dir, 'PluginBenchmark')
+    build_dir = os.path.join(project_dir, 'build')
+    if not os.path.exists(build_dir):
+        os.makedirs(build_dir)
+    buildConfig(script_dir, project_dir, cuda_path)
+    print_green("Build Benchmark Plugin...")
+    compileProjects(project_dir, "Debug")
+    compileProjects(project_dir, "Release")
+
+
+def build_unity(script_dir, unity_bin, num_samples):
+    unity_dir = os.path.join(script_dir, 'UnityBenchmark')
+    print_green(
+        "Build Unity project with OpenGL... It might take a few seconds...")
+    unity_build_dir_gl = os.path.join(script_dir, 'build', 'UnityBenchmark-GL')
+    os.makedirs(unity_build_dir_gl, exist_ok=True)
+    subprocess.run([unity_bin, '-projectPath', unity_dir, '-buildTarget', 'Win64', '-executeMethod',
+                    'BuildScript.BuildGL', '-logFile', '-', '-quit', '-batchmode'])
+    source_gl_build_dir = os.path.join(unity_dir, 'build', 'GLBuild')
+    if os.path.exists(source_gl_build_dir):
+        for item in os.listdir(source_gl_build_dir):
+            s = os.path.join(source_gl_build_dir, item)
+            d = os.path.join(unity_build_dir_gl, item)
+            if os.path.isdir(s):
+                shutil.copytree(s, d, dirs_exist_ok=True)
+            else:
+                shutil.copy2(s, d)
+        create_config_file(unity_build_dir_gl, num_samples)
+
+    print_green(
+        "Build Unity project with DirectX11... It might take a few seconds...")
+    unity_build_dir_dx11 = os.path.join(
+        script_dir, 'build', 'UnityBenchmark-DX11')
+    os.makedirs(unity_build_dir_dx11, exist_ok=True)
+    subprocess.run([unity_bin, '-projectPath', unity_dir, '-buildTarget', 'Win64', '-executeMethod',
+                    'BuildScript.BuildDX11', '-logFile', '-', '-quit', '-batchmode'])
+    source_dx11_build_dir = os.path.join(unity_dir, 'build', 'DX11Build')
+    if os.path.exists(source_dx11_build_dir):
+        for item in os.listdir(source_dx11_build_dir):
+            s = os.path.join(source_dx11_build_dir, item)
+            d = os.path.join(unity_build_dir_dx11, item)
+            if os.path.isdir(s):
+                shutil.copytree(s, d, dirs_exist_ok=True)
+            else:
+                shutil.copy2(s, d)
+        create_config_file(unity_build_dir_dx11, num_samples)
+
+
+def benchmark(script_dir, num_tests, batchmode):
+    for i in range(num_tests):
+        if batchmode:
+            print_green(
+                "Launch benchmark OpenGL in batchmode... It might takes several minutes")
+        else:
+            print_green("Launch benchmark OpenGL...")
+        benchmark_gl_path = os.path.join(
+            script_dir, 'build', 'UnityBenchmark-GL', 'UnityBenchmark.exe')
+        if os.path.exists(benchmark_gl_path):
+            benchmark_gl = subprocess.run(
+                [benchmark_gl_path, '-batchmode' if batchmode else ''])
+            if benchmark_gl.returncode == 0:
+                results_dir_gl = os.path.join(
+                    script_dir, 'Results', f'UnityBenchmark-GL-{i}')
+                os.makedirs(results_dir_gl, exist_ok=True)
+                for file in os.listdir(os.path.join(script_dir, 'build', 'UnityBenchmark-GL', 'UnityBenchmark_Data')):
+                    if file.endswith('.csv'):
+                        shutil.copy(os.path.join(script_dir, 'build', 'UnityBenchmark-GL', 'UnityBenchmark_Data', file),
+                                    results_dir_gl)
+        else:
+            print_red(
+                f"Error: {benchmark_gl_path} does not exist. Build the unity project before launching benchmark.")
+            return
+
+        if batchmode:
+            print_green(
+                "Launch benchmark DirectX11 in batchmode... It might takes several minutes")
+        else:
+            print_green("Launch benchmark DirectX11...")
+        benchmark_dx11_path = os.path.join(
+            script_dir, 'build', 'UnityBenchmark-DX11', 'UnityBenchmark.exe')
+        if os.path.exists(benchmark_dx11_path):
+            benchmark_dx11 = subprocess.run(
+                [benchmark_dx11_path, '-batchmode' if batchmode else ''])
+            if benchmark_dx11.returncode == 0:
+                results_dir_dx11 = os.path.join(
+                    script_dir, 'Results', f'UnityBenchmark-DX11-{i}')
+                os.makedirs(results_dir_dx11, exist_ok=True)
+                for file in os.listdir(os.path.join(script_dir, 'build', 'UnityBenchmark-DX11', 'UnityBenchmark_Data')):
+                    if file.endswith('.csv'):
+                        shutil.copy(os.path.join(script_dir, 'build', 'UnityBenchmark-DX11', 'UnityBenchmark_Data', file),
+                                    results_dir_dx11)
+        else:
+            print_red(
+                f"Error: {benchmark_dx11_path} does not exist. Build the unity project before launching benchmark.")
+            return
+
+
+def draw(script_dir):
+    print_green("Create and save the graph of comparison")
+    for result_dir in os.listdir(os.path.join(script_dir, 'Results')):
+        result_path = os.path.join(script_dir, 'Results', result_dir)
+        if os.path.isdir(result_path):
+            for csv_file in os.listdir(result_path):
+                if csv_file.endswith('.csv'):
+                    csv_path = os.path.join(result_path, csv_file)
+                    draw_args = ['python', os.path.join(
+                        script_dir, 'Results', 'main.py'), '-i', csv_path, '-t', csv_path, '-s', 'False']
+                    subprocess.run(draw_args)
+
+
 def main():
     parser = argparse.ArgumentParser(description='Build configuration script.')
     parser.add_argument("-c", "--cuda-path", default="C://Program Files//NVIDIA GPU Computing Toolkit//CUDA//v12.3",
@@ -67,133 +202,27 @@ def main():
                         help='If true will display the benchmark, otherwise it won\'t')
     parser.add_argument("-n", "--number-tests", type=int, default=1,
                         help='Number of times the benchmark should be launched')
+    parser.add_argument("-ns", "--number-samples", type=int, default=1000,
+                        help='Number of samples per size to update the Config.json')
     args = parser.parse_args()
 
     # Get the directory of the script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     if args.package_interop:
-        print_green("Package InteropUnityCUDA...")
-
-        interop_unity_cuda_dir = os.path.join(
-            script_dir, 'PluginBenchmark', 'Vendors', 'InteropUnityCUDA')
-        package_script_path = os.path.join(
-            interop_unity_cuda_dir, 'Plugin', 'buildtools', 'packageUnity.py')
-        if os.path.exists(interop_unity_cuda_dir):
-            packaged_args = ['python', package_script_path]
-            print_green("Execute package script: ", packaged_args)
-            run_command(packaged_args, cwd=script_dir)
+        package_interop(script_dir)
 
     if args.plugin:
-        print_green("Configure Benchmark Plugin...")
-        project_dir = os.path.join(script_dir, 'PluginBenchmark')
-
-        build_dir = os.path.join(project_dir, 'build')
-        if not os.path.exists(build_dir):
-            os.makedirs(build_dir)
-        buildConfig(script_dir, project_dir, args.cuda_path)
-
-        print_green("Build Benchmark Plugin...")
-        compileProjects(project_dir, "Debug")
-        compileProjects(project_dir, "Release")
+        generate_compile_plugin(script_dir, args.cuda_path)
 
     if args.build_unity:
-        unity_dir = os.path.join(script_dir, 'UnityBenchmark')
-        print_green(
-            "Build Unity project with OpenGL... It might take a few seconds...")
-        unity_build_dir_gl = os.path.join(
-            script_dir, 'build', 'UnityBenchmark-GL')
-        os.makedirs(unity_build_dir_gl, exist_ok=True)
-        subprocess.run([args.unityBin, '-projectPath', unity_dir, '-buildTarget', 'Win64', '-executeMethod',
-                       'BuildScript.BuildGL', '-logFile', '-', '-quit', '-batchmode'])
-
-        source_gl_build_dir = os.path.join(unity_dir, 'build', 'GLBuild')
-        if os.path.exists(source_gl_build_dir):
-            for item in os.listdir(source_gl_build_dir):
-                s = os.path.join(source_gl_build_dir, item)
-                d = os.path.join(unity_build_dir_gl, item)
-                if os.path.isdir(s):
-                    shutil.copytree(s, d, dirs_exist_ok=True)
-                else:
-                    shutil.copy2(s, d)
-
-        print_green(
-            "Build Unity project with DirectX11... It might take a few seconds...")
-        unity_build_dir_dx11 = os.path.join(
-            script_dir, 'build', 'UnityBenchmark-DX11')
-        os.makedirs(unity_build_dir_dx11, exist_ok=True)
-        subprocess.run([args.unityBin, '-projectPath', unity_dir, '-buildTarget', 'Win64', '-executeMethod',
-                       'BuildScript.BuildDX11', '-logFile', '-', '-quit', '-batchmode'])
-
-        source_dx11_build_dir = os.path.join(unity_dir, 'build', 'DX11Build')
-        if os.path.exists(source_dx11_build_dir):
-            for item in os.listdir(source_dx11_build_dir):
-                s = os.path.join(source_dx11_build_dir, item)
-                d = os.path.join(unity_build_dir_dx11, item)
-                if os.path.isdir(s):
-                    shutil.copytree(s, d, dirs_exist_ok=True)
-                else:
-                    shutil.copy2(s, d)
+        build_unity(script_dir, args.unityBin, args.number_samples)
 
     if args.benchmark:
-        for i in range(args.number_tests):
-            if (args.batchmode):
-                print_green(
-                    "Launch benchmark OpenGL in batchmode... It might takes several minutes")
-            else:
-                print_green("Launch benchmark OpenGL...")
-            benchmark_gl_path = os.path.join(
-                script_dir, 'build', 'UnityBenchmark-GL', 'UnityBenchmark.exe')
-            if os.path.exists(benchmark_gl_path):
-                benchmark_gl = subprocess.run(
-                    [benchmark_gl_path, '-batchmode' if args.batchmode else ''])
-                if benchmark_gl.returncode == 0:
-                    results_dir_gl = os.path.join(
-                        script_dir, 'Results', f'UnityBenchmark-GL-{i}')
-                    os.makedirs(results_dir_gl, exist_ok=True)
-                    for file in os.listdir(os.path.join(script_dir, 'build', 'UnityBenchmark-GL', 'UnityBenchmark_Data')):
-                        if file.endswith('.csv'):
-                            shutil.copy(os.path.join(
-                                script_dir, 'build', 'UnityBenchmark-GL', 'UnityBenchmark_Data', file), results_dir_gl)
-            else:
-                print_red(
-                    f"Error: {benchmark_gl_path} does not exist. Build the unity project before launching benchmark.")
-                return
-
-            if (args.batchmode):
-                print_green(
-                    "Launch benchmark DirectX11 in batchmode... It might takes several minutes")
-            else:
-                print_green("Launch benchmark DirectX11...")
-            benchmark_dx11_path = os.path.join(
-                script_dir, 'build', 'UnityBenchmark-DX11', 'UnityBenchmark.exe')
-            if os.path.exists(benchmark_dx11_path):
-                benchmark_dx11 = subprocess.run(
-                    [benchmark_dx11_path, '-batchmode' if args.batchmode else ''])
-                if benchmark_dx11.returncode == 0:
-                    results_dir_dx11 = os.path.join(
-                        script_dir, 'Results', f'UnityBenchmark-DX11-{i}')
-                    os.makedirs(results_dir_dx11, exist_ok=True)
-                    for file in os.listdir(os.path.join(script_dir, 'build', 'UnityBenchmark-DX11', 'UnityBenchmark_Data')):
-                        if file.endswith('.csv'):
-                            shutil.copy(os.path.join(
-                                script_dir, 'build', 'UnityBenchmark-DX11', 'UnityBenchmark_Data', file), results_dir_dx11)
-            else:
-                print_red(
-                    f"Error: {benchmark_dx11_path} does not exist. Build the unity project before launching benchmark.")
-                return
+        benchmark(script_dir, args.number_tests, args.batchmode)
 
     if args.draw:
-        print_green("Create and save the graph of comparison")
-        for result_dir in os.listdir(os.path.join(script_dir, 'Results')):
-            result_path = os.path.join(script_dir, 'Results', result_dir)
-            if os.path.isdir(result_path):
-                for csv_file in os.listdir(result_path):
-                    if csv_file.endswith('.csv'):
-                        csv_path = os.path.join(result_path, csv_file)
-                        draw_args = ['python', os.path.join(
-                            script_dir, 'Results', 'main.py'), '-i', csv_path, '-t', csv_path, '-s', 'False']
-                        subprocess.run(draw_args)
+        draw(script_dir)
 
 
 if __name__ == '__main__':
