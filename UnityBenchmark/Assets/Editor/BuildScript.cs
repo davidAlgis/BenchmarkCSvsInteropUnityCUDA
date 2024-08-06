dc4dcf41a568cdd391ce31abd4cf4299c97fcda1
david.algis <david.algis@studio-nyx.com>
2024-08-06 17:28:09 +0200

    🐛🔨 Fixed build script to correctly set graphics API

--- UnityBenchmark/Assets/Editor/BuildScript.cs
+++ UnityBenchmark/Assets/Editor/BuildScript.cs
@@ -1,12 +1,16 @@
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class BuildScript
 {
     [MenuItem("Build/BuildGL")]
     public static void BuildGL()
     {
+        // Set the graphics API to OpenGLCore
+        PlayerSettings.SetGraphicsAPIs(BuildTarget.StandaloneWindows64, new[] { GraphicsDeviceType.OpenGLCore });
+
         string[] scenes = GetScenesFromBuildSettings();
         BuildPipeline.BuildPlayer(scenes, "build/GLBuild/UnityBenchmark.exe", BuildTarget.StandaloneWindows64,
             BuildOptions.None);
@@ -16,6 +20,9 @@ public class BuildScript
     [MenuItem("Build/BuildDX11")]
     public static void BuildDX11()
     {
+        // Set the graphics API to DirectX11
+        PlayerSettings.SetGraphicsAPIs(BuildTarget.StandaloneWindows64, new[] { GraphicsDeviceType.Direct3D11 });
+
         string[] scenes = GetScenesFromBuildSettings();
         BuildPipeline.BuildPlayer(scenes, "build/DX11Build/UnityBenchmark.exe", BuildTarget.StandaloneWindows64,
             BuildOptions.None);
